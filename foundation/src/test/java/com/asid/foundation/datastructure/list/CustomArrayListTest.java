857902c4e8449e3f805931a709c90f34d3290533
TnT8 <jacek.gajewski@ymail.com>
2017-03-08 18:42:28 +0100

    Test jednostkowy

--- foundation/src/test/java/com/asid/foundation/datastructure/list/CustomArrayListTest.java
+++ foundation/src/test/java/com/asid/foundation/datastructure/list/CustomArrayListTest.java
@@ -6,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Example for students
@@ -13,14 +14,16 @@ import java.util.List;
  */
 public class CustomArrayListTest {
     List<Integer> list = null;
+    List<Object> listObject = null;
 
     @Before
     public void setup() {
         list = new CustomArrayList();
+        listObject = new CustomArrayList();
+
     }
 
     @Test
-    @Ignore /* remove this line and write other tests. */
     public void shouldAddOneElement() {
         // Pre
         list.clear();
@@ -33,5 +36,78 @@ public class CustomArrayListTest {
         Assert.assertEquals(2, list.size());
         Assert.assertEquals(2, (int) list.get(0));
         Assert.assertEquals(3, (int) list.get(1));
+
     }
+    @Test
+    public void shouldBeEmpty(){
+        //Pre
+        list.clear();
+
+        //Assert
+        Assert.assertEquals(true, list.isEmpty());
+    }
+    @Test
+    public void shouldGetIndex(){
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+
+        //Assert
+        Assert.assertEquals(1, listObject.indexOf(b));
+    }
+
+    @Test
+    public void shouldContain(){
+        //Pre
+        listObject.clear();
+        Object a = new CustomLinkedList<>();
+
+        //Act
+        listObject.add(a);
+
+        //Assert
+        Assert.assertEquals(true, listObject.contains(a));
+    }
+    @Test
+    public void shouldSet() {
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+        listObject.set(1, c);
+
+        //Assert
+        Assert.assertEquals(c, listObject.get(1));
+    }
+        @Test
+        public void shouldRemove(){
+
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+        listObject.set(1, c);
+        listObject.remove(c);
+        listObject.remove(0);
+
+        Assert.assertEquals(0, list.size());
+        }
+
+
 }
---
+++ foundation/src/test/java/com/asid/foundation/datastructure/list/CustomLinkedListTest.java
@@ -0,0 +1,110 @@
+package com.asid.foundation.datastructure.list;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+public class CustomLinkedListTest {
+
+
+    List<Integer> list = null;
+    List<Object> listObject = null;
+
+
+    @Before
+    public void setup() {
+        list = new CustomLinkedList();
+        listObject = new CustomLinkedList();
+
+    }
+
+    @Test
+    public void shouldAddOneElement() {
+        // Pre
+        list.clear();
+
+        //Act
+        list.add(2);
+        list.add(3);
+
+        //Assert
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(2, (int) list.get(0));
+        Assert.assertEquals(3, (int) list.get(1));
+
+    }
+    @Test
+    public void shouldBeEmpty(){
+
+        list.clear();
+
+        Assert.assertEquals(true, list.isEmpty());
+    }
+    @Test
+    public void shouldGetIndex() {
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+
+        //Assert
+        Assert.assertEquals(1, listObject.indexOf(b));
+    }
+        @Test
+        public void shouldContain(){
+            //Pre
+            listObject.clear();
+            Object a = new CustomLinkedList<>();
+
+            //Act
+            listObject.add(a);
+
+            //Assert
+            Assert.assertEquals(true, listObject.contains(a));
+        }
+    @Test
+    public void shouldSet() {
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+        listObject.set(1, c);
+
+        //Assert
+        Assert.assertEquals(c, listObject.get(1));
+    }
+    @Test
+    public void shouldRemove(){
+
+        //Pre
+        listObject.clear();
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        //Act
+        listObject.add(a);
+        listObject.add(b);
+        listObject.set(1, c);
+        listObject.remove(c);
+        listObject.remove(0);
+
+        Assert.assertEquals(0, list.size());
+    }
+
+
+
+}

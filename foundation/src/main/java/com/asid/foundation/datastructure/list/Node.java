cdb1c38c8bb1233930a2e7e5de97c23922cce069
TnT8 <jacek.gajewski@ymail.com>
2017-03-09 21:32:10 +0100

    Node class

--- foundation/src/main/java/com/asid/foundation/datastructure/list/Node.java
+++ foundation/src/main/java/com/asid/foundation/datastructure/list/Node.java
@@ -4,5 +4,31 @@ package com.asid.foundation.datastructure.list;
  * Component of linked list that stores a value and reference to the next element.
  */
 public class Node {
-      /* (TODO Lab No. 1) Please introduce a sensible implementation */
+
+    private Node next;
+    private Object data;
+
+    public Node(Object data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(Object data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public Object getData(){
+        return data;
+    }
+    public Node getNext(){
+        return next;
+    }
+    public void setData(Object data){
+        this.data = data;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+
 }

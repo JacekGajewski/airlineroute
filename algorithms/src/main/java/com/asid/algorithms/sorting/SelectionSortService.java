9b69d0c1bc508426fbd17c7e722dc8f6ee3415ea
TnT8 <jacek.gajewski@ymail.com>
2017-03-24 20:15:00 +0100

    Insertion Sort

--- algorithms/src/main/java/com/asid/algorithms/sorting/SelectionSortService.java
+++ algorithms/src/main/java/com/asid/algorithms/sorting/SelectionSortService.java
@@ -2,6 +2,7 @@ package com.asid.algorithms.sorting;
 
 import com.asid.foundation.messages.StandardMessages;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,9 +13,26 @@ public class SelectionSortService extends AbstractSortService {
 
     public static <T> SortResultDs<T> sort(List<T> list, Comparator<? super T> comparator) {
 
+        long startTime = System.currentTimeMillis();
+        SortResultDs<T> resultDs = new SortResultDs<>();
+        List<T> temp = new ArrayList<>();
+        T sth = list.get(0);
+        for(int i = 0; i<list.size(); i++) {
+            for (T t : list) {
+                if (comparator.compare(t, sth) == 1) {
+                    sth = t;
+                }
+            }
+            temp.add(i, sth);
+        }
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        resultDs.setResult(temp);
+        resultDs.setEstimatedTime(elapsedTime);
 
+        return resultDs;
 
-
-        throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+        //throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
     }
 }
+

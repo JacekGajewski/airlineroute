f27f6a71745f963d500ae09f1614d9eb52e00adb
TnT8 <jacek.gajewski@ymail.com>
2017-03-25 14:56:35 +0100

    Insertion Sort

--- algorithms/src/main/java/com/asid/algorithms/sorting/InsertionSortService.java
+++ algorithms/src/main/java/com/asid/algorithms/sorting/InsertionSortService.java
@@ -2,7 +2,9 @@ package com.asid.algorithms.sorting;
 
 import com.asid.foundation.messages.StandardMessages;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,7 +13,37 @@ import java.util.List;
 public class InsertionSortService extends AbstractSortService {
 
     public static <T> SortResultDs<T> sort(List<T> list, Comparator<? super T> comparator) {
-          /* (TODO Lab No. 3) Please introduce a sensible implementation */
-        throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+
+        SortResultDs sortResultDs = new SortResultDs();
+        List<T> temp = new LinkedList<>();
+
+        if(comparator.compare(list.get(0), list.get(1)) == 1){
+            temp.add(0, list.get(1));
+            temp.add(1, list.get(0));
+        }else{
+            temp.add(0, list.get(0));
+            temp.add(1, list.get(1));
+        }
+
+        for(T l : list){
+            for(int i = 0; i < temp.size(); i++){
+                if(comparator.compare(l , temp.get(0)) == -1) {
+                    temp.add(0, l);
+                    break;
+                }else if(comparator.compare(l , temp.get(i)) == 1 &&
+                        i + 1 == temp.size()){
+                    temp.add(i + 1, l);
+                    break;
+
+                }else if(comparator.compare(l , temp.get(i)) == 1 &&
+                        comparator.compare(l , temp.get(i + 1)) == -1){
+                    temp.add(i + 1, l);
+                    break;
+                }
+            }
+        }
+        sortResultDs.setResult(temp);
+        return sortResultDs;
+        //throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
     }
 }

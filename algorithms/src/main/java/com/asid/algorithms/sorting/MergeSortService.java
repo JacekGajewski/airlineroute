d3254705ca3757e17f49de2f7f7a75e4026278e2
TnT8 <jacek.gajewski@ymail.com>
2017-03-28 22:02:52 +0200

    MergeSort

--- algorithms/src/main/java/com/asid/algorithms/sorting/MergeSortService.java
+++ algorithms/src/main/java/com/asid/algorithms/sorting/MergeSortService.java
@@ -2,7 +2,9 @@ package com.asid.algorithms.sorting;
 
 import com.asid.foundation.messages.StandardMessages;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,8 +12,65 @@ import java.util.List;
  */
 public class MergeSortService extends AbstractSortService {
 
+    static long startTime = System.currentTimeMillis();
+
     public static <T> SortResultDs<T> sort(List<T> list, Comparator<? super T> comparator) {
-          /* (TODO Lab No. 4) Please introduce a sensible implementation */
-        throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+
+        SortResultDs<T> sortResultDs = new SortResultDs<>();
+        List<T> temp = new ArrayList<>();
+
+        int n = list.size();
+        if(n < 2){
+            temp.add(list.get(0));
+            sortResultDs.setResult(temp);
+            return sortResultDs;
+        }
+        List<T> left = new ArrayList<>();
+        List<T> right = new ArrayList<>();
+
+        int length = list.size()/2;
+        int rest = list.size() - length;
+
+        for (int i = 0; i < length; i++){
+            left.add(list.get(i));
+        }
+        for (int i = 0; i < rest; i++){
+            right.add(list.get(length + i));
+        }
+
+        sortResultDs.setResult(merge(sort(left, comparator).getResult(), sort(right, comparator).getResult(), comparator));
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        sortResultDs.setEstimatedTime(elapsedTime);
+        return sortResultDs;
+
+
+        //throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+    }
+
+    public static <T>List<T> merge(List<T> leftList, List<T> rightList, Comparator<? super T> comparator){
+
+        List<T> result = new LinkedList<>();
+        int i = 0;
+        int j = 0;
+        while(i < leftList.size() && j < rightList.size()){
+            if(comparator.compare(leftList.get(i), rightList.get(j)) == 1 ||
+                    comparator.compare(leftList.get(i), rightList.get(j)) == 0){
+                result.add(rightList.get(j));
+                j++;
+            }else{
+                result.add(leftList.get(i));
+                i++;
+            }
+        }
+        while(i < leftList.size()){
+           result.add(leftList.get(i));
+           i++;
+        }
+        while(j < rightList.size()){
+            result.add(rightList.get(j));
+            j++;
+        }
+        return result;
     }
 }
--- apps/src/main/java/com/aisd/apps/sorting/SortingAlgorithmComparison.java
+++ apps/src/main/java/com/aisd/apps/sorting/SortingAlgorithmComparison.java
@@ -1,12 +1,168 @@
 package com.aisd.apps.sorting;
 
+import com.asid.algorithms.database.DataLoader;
+import com.asid.algorithms.database.DataLoaderImpl;
+import com.asid.algorithms.database.InmemmoryDataBase;
+import com.asid.algorithms.entity.Route;
+import com.asid.algorithms.mapper.RouteMapper;
+import com.asid.algorithms.sorting.InsertionSortService;
+import com.asid.algorithms.sorting.MergeSortService;
+import com.asid.algorithms.sorting.SelectionSortService;
+import com.asid.algorithms.sorting.SortResultDs;
+
+import java.util.*;
+
 /**
  * Sorting algorithms performance test.
  */
 public class SortingAlgorithmComparison {
 
     public static void main(String[] args) {
-         /* (TODO Lab No. 3) Please introduce a sensible implementation */
+
+        Comparator<Route> comparator = getComparator();
+        List<Route> list1 = getTarget();
+
+        InsertionSortService insertionSortService = new InsertionSortService();
+        SortResultDs sortResultDs = insertionSortService.sort(list1, comparator);
+        System.out.println("InsertSort: " + sortResultDs.getEstimatedTime());
+
+
+        List<Route> list2 = getTarget();
+
+        SelectionSortService selectionSortService = new SelectionSortService();
+        SortResultDs selectionSort = selectionSortService.sort(list2, comparator);
+        System.out.println("SelectionSort: " + selectionSort.getEstimatedTime());
+
+        List<Route> list5 = getTarget();
+
+        MergeSortService mergeSortService = new MergeSortService();
+        SortResultDs mergeSort = mergeSortService.sort(list5, comparator);
+        System.out.println("MergeSort: " + mergeSort.getEstimatedTime());
+
+
+        List<Route> list3 = getTarget();
+        Collections.sort(list3, getComparator());
+        System.out.println("POSORTOWANE OPTYMISTYCZNIE");
+
+        SortResultDs sortResultDs3 = insertionSortService.sort(list3, comparator);
+        System.out.println("InsertSort: " + sortResultDs3.getEstimatedTime());
+        SortResultDs mergeSort3 = mergeSortService.sort(list3, comparator);
+        System.out.println("MergeSort: " + mergeSort3.getEstimatedTime());
+        SortResultDs selectionSort3 = selectionSortService.sort(list3, comparator);
+        System.out.println("SelectionSort: " + selectionSort3.getEstimatedTime());
+
+        List<Route> list4 = getTarget();
+        Collections.sort(list4, getComparatorReverse());
+        System.out.println("POSORTOWANE PESYMISTYCZNIE");
+
+        SortResultDs sortResultDs4 = insertionSortService.sort(list4, comparator);
+        System.out.println("InsertSort: " + sortResultDs4.getEstimatedTime());
+        SortResultDs mergeSort4 = mergeSortService.sort(list4, comparator);
+        System.out.println("MergeSort: " + mergeSort4.getEstimatedTime());
+        SortResultDs selectionSort4 = selectionSortService.sort(list4, comparator);
+        System.out.println("SelectionSort: " + selectionSort4.getEstimatedTime());
+
+    }
+
+
+
+    public static List<Route> getTarget(){
+
+        DataLoader loader = new DataLoaderImpl();
+
+        loader.loadAllData("/home/tnt9/Downloads/airlineroute/algorithms/resources/airports.dat",
+                "/home/tnt9/Downloads/airlineroute/algorithms/resources/airlines.dat",
+                "/home/tnt9/Downloads/airlineroute/algorithms/resources/routes.dat");
+
+        RouteMapper routeMapper = new RouteMapper();
+        List<Route> target = new LinkedList<>();
+
+        routeMapper.toRoutes(InmemmoryDataBase.getInstance().getRoutes(), target); // Nie ładuje ostatniego
+
+        return target;
+    }
+
+    public static Comparator<Route> getComparator(){
+        Comparator<Route> comparator = new Comparator<Route>() {
+            @Override
+            public int compare(Route o1, Route o2) {
+                if(Math.sqrt(Math.pow((o1.getOrigin().getLongitude() - o1.getDestination().getLongitude()), 2) +
+                        Math.pow((o1.getOrigin().getLatitude() - o1.getDestination().getLatitude()), 2)) >
+                        Math.sqrt(Math.pow((o2.getOrigin().getLongitude() - o2.getDestination().getLongitude()), 2) +
+                                Math.pow((o2.getOrigin().getLatitude() - o2.getDestination().getLatitude()), 2))) {
+                    return 1;
+                }else if(Math.sqrt(Math.pow((o1.getOrigin().getLongitude() - o1.getDestination().getLongitude()), 2) +
+                        Math.pow((o1.getOrigin().getLatitude() - o1.getDestination().getLatitude()), 2)) <
+                        Math.sqrt(Math.pow((o2.getOrigin().getLongitude() - o2.getDestination().getLongitude()), 2) +
+                                Math.pow((o2.getOrigin().getLatitude() - o2.getDestination().getLatitude()), 2))){
+                    return -1;
+                }
+                return 0;
+            }
+        };
+
+        return comparator;
+    }
+    public static Comparator<Route> getComparatorReverse(){
+        Comparator<Route> comparator = new Comparator<Route>() {
+            @Override
+            public int compare(Route o1, Route o2) {
+                if(Math.sqrt(Math.pow((o1.getOrigin().getLongitude() - o1.getDestination().getLongitude()), 2) +
+                        Math.pow((o1.getOrigin().getLatitude() - o1.getDestination().getLatitude()), 2)) >
+                        Math.sqrt(Math.pow((o2.getOrigin().getLongitude() - o2.getDestination().getLongitude()), 2) +
+                                Math.pow((o2.getOrigin().getLatitude() - o2.getDestination().getLatitude()), 2))) {
+                    return -1;
+                }else if(Math.sqrt(Math.pow((o1.getOrigin().getLongitude() - o1.getDestination().getLongitude()), 2) +
+                        Math.pow((o1.getOrigin().getLatitude() - o1.getDestination().getLatitude()), 2)) <
+                        Math.sqrt(Math.pow((o2.getOrigin().getLongitude() - o2.getDestination().getLongitude()), 2) +
+                                Math.pow((o2.getOrigin().getLatitude() - o2.getDestination().getLatitude()), 2))){
+                    return 1;
+                }
+                return 0;
+            }
+        };
+
+        return comparator;
+    }
+    public static Comparator<Route> newComparator(){
+        Comparator<Route> comparator = new Comparator<Route>() {
+            @Override
+            public int compare(Route o1, Route o2) {
+                if(o1.getOrigin().getLongitude() > o2.getOrigin().getLongitude()){
+
+                    return 1;
+                }else if(o1.getOrigin().getLongitude() < o2.getOrigin().getLongitude()){
+                    return -1;
+                }
+                return 0;
+            }
+        };
+
+        return comparator;
+    }
+    public static Comparator<Route> newReverseComparator(){
+        Comparator<Route> comparator = new Comparator<Route>() {
+            @Override
+            public int compare(Route o1, Route o2) {
+                if(o1.getOrigin().getLongitude() > o2.getOrigin().getLongitude()){
+
+                    return -1;
+                }else if(o1.getOrigin().getLongitude() < o2.getOrigin().getLongitude()){
+                    return 1;
+                }
+                return 0;
+            }
+        };
+
+        return comparator;
+    }
+
+
+
+
+    public <T> List<Route> sort(List<T> list, Comparator<? super T> comparator){
+
+        return null;
     }
 
 

e7eb63f83e9ef1201a31c1d7a1e8151125d12b12
JacekGajewski <jacek.gajewski@ymail.com>
2017-05-19 20:00:26 +0200

    Kruskas algorithm

--- algorithms/src/test/java/com/asid/algorithms/graph/MinimumSpanningTreeServiceTest.java
+++ algorithms/src/test/java/com/asid/algorithms/graph/MinimumSpanningTreeServiceTest.java
@@ -33,6 +33,65 @@ public class MinimumSpanningTreeServiceTest {
             Assert.assertEquals(3, minimumSpanningTreeService.searchMinimumSpanningTreeUsingPrimAlg(customUndirectedWeightGraphAdapter).getTotalWeight(), 0.01);
             Assert.assertEquals(2, minimumSpanningTreeService.searchMinimumSpanningTreeUsingPrimAlg(customUndirectedWeightGraphAdapter).getEgdes().size());
 
+            Integer three = new Integer(3);
+            Integer four = new Integer(4);
+            Integer five = new Integer(5);
+            Integer six = new Integer(6);
+            customUndirectedWeightGraphAdapter.addVertex(three);
+            customUndirectedWeightGraphAdapter.addVertex(four);
+            customUndirectedWeightGraphAdapter.addVertex(five);
+            customUndirectedWeightGraphAdapter.addVertex(six);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(one, three), 4);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(two, four), 7);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, six), 6);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, four), 5);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, three), 4);
+            customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(six, four), 7);
+
+            Assert.assertEquals(28, minimumSpanningTreeService.searchMinimumSpanningTreeUsingPrimAlg(customUndirectedWeightGraphAdapter).getTotalWeight(), 0.01);
+            Assert.assertEquals(6, minimumSpanningTreeService.searchMinimumSpanningTreeUsingPrimAlg(customUndirectedWeightGraphAdapter).getEgdes().size());
+
         }
+    @Test
+    public void KruskaAlg() {
+
+        AirportEdgeFactory airportEdgeFactory = new AirportEdgeFactory();
+
+        CustomUndirectedWeightGraphAdapter customUndirectedWeightGraphAdapter = new CustomUndirectedWeightGraphAdapter(airportEdgeFactory, true);
+
+        Integer zero = new Integer(0);
+        Integer one = new Integer(1);
+        Integer two = new Integer(2);
+        customUndirectedWeightGraphAdapter.addVertex(zero);
+        customUndirectedWeightGraphAdapter.addVertex(one);
+        customUndirectedWeightGraphAdapter.addVertex(two);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(one, two), 3);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(one, zero), 1);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(zero, two), 2);
+
+        MinimumSpanningTreeService minimumSpanningTreeService = new MinimumSpanningTreeService();
+
+        Assert.assertEquals(3, minimumSpanningTreeService.searchMinimumSpanningTreeUsingKruskalAlg(customUndirectedWeightGraphAdapter).getTotalWeight(), 0.01);
+        Assert.assertEquals(2, minimumSpanningTreeService.searchMinimumSpanningTreeUsingKruskalAlg(customUndirectedWeightGraphAdapter).getEgdes().size());
+
+        Integer three = new Integer(3);
+        Integer four = new Integer(4);
+        Integer five = new Integer(5);
+        Integer six = new Integer(6);
+        customUndirectedWeightGraphAdapter.addVertex(three);
+        customUndirectedWeightGraphAdapter.addVertex(four);
+        customUndirectedWeightGraphAdapter.addVertex(five);
+        customUndirectedWeightGraphAdapter.addVertex(six);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(one, three), 4);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(two, four), 7);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, six), 6);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, four), 5);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(five, three), 4);
+        customUndirectedWeightGraphAdapter.setEdgeWeight(customUndirectedWeightGraphAdapter.addEdge(six, four), 7);
+
+        Assert.assertEquals(28, minimumSpanningTreeService.searchMinimumSpanningTreeUsingKruskalAlg(customUndirectedWeightGraphAdapter).getTotalWeight(), 0.01);
+        Assert.assertEquals(6, minimumSpanningTreeService.searchMinimumSpanningTreeUsingKruskalAlg(customUndirectedWeightGraphAdapter).getEgdes().size());
+
+    }
 
 }

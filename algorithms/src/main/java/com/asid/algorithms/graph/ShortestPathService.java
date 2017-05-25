dfaabcac8d63eb803141c7f826bab14900fe049b
JacekGajewski <jacek.gajewski@ymail.com>
2017-05-25 21:23:02 +0200

    Initialization of ShortestPath

---
+++ algorithms/src/main/java/com/asid/algorithms/graph/ShortestPathService.java
@@ -0,0 +1,18 @@
+package com.asid.algorithms.graph;
+
+import com.asid.foundation.datastructure.graph.AbstractUndirectedWeightGraphAdapter;
+
+/**
+ * Created by tnt9 on 25.05.17.
+ */
+public class ShortestPathService implements AbstractShortestPathService {
+    @Override
+    public MSTResultDs searchShortestPathUsingDijkstraAlg(AbstractUndirectedWeightGraphAdapter graph, Object from, Object to) {
+        return null;
+    }
+
+    @Override
+    public MSTResultDs searchShortestPathUsingBellmanFordAlg(AbstractUndirectedWeightGraphAdapter graph, Object from, Object to) {
+        return null;
+    }
+}
